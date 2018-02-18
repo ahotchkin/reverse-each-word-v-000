13ff2104f2d980e11ac58a2905e32e7fe2152fda
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-18 15:27:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.each { |i| i.reverse }
+  new_array = []
+  array.each { |i| new_array << i.reverse }
   new_string = array.join(" ")
 end
